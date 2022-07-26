ef57818112403b9325175f9ffc64b8c29da50cb6
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-26 07:18:24 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 export default function App() {
   return (
     <BrowserRouter>
-      
+      <Header />
       <Routes>
         <Route path="" element={<h1>home</h1>} />
         <Route path="login" element={<h1>Login</h1>} />
@@ -14,5 +14,19 @@ export default function App() {
         <Route path="Errorpage" element={<h1>Errorpage</h1>} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">home</Link>
+      <Link to="/Login">Login</Link>
+      <Link to="/Signup">Signup</Link>
+      <Link to="/Cart">Cart</Link>
+      <Link to="/Orders">Orders</Link>
+      <Link to="/Profile">Profile</Link>
+      <Link to="/Errorpage">Errorpage</Link>
+    </header>
   );
 }
